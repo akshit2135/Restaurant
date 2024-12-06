cc5f759e863c44afed33dd02f49056da10089b05
akshit2135 <162620254+akshit2135@users.noreply.github.com>
2024-12-06 19:25:31 +0530

    update

---
+++ restaurant-website/src/data/data.js
@@ -0,0 +1,13 @@
+import Dosa from '../images/dosa.jpg'
+import Chola from '../images/chhola.jpg'
+import gujrati from '../images/gujrati.jpeg'
+import idli from '../images/idli.jpg'
+import masala from '../images/masala.jpg'
+import paneer from '../images/paneer.jpg'
+
+
+export const MenuList = {
+    {
+        name: 'Dosa'                  
+    }
+}
